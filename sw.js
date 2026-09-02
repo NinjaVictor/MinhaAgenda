58a7844a99672d8d822240f7d62c3cfc70e97839
Espanhol <117646176+NinjaVictor@users.noreply.github.com>
2026-09-02 12:28:39 -0300

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE="minha-agenda-v2";
+
+const FILES=[
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      return cached || fetch(event.request);
+    })
+  );
+});
